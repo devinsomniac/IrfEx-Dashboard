52cd54b9b930a94ab12cf12903b653b6759c5e3c
devinsomniac <inzamamchowdhury20@gmail.com>
2025-01-23 03:00:46 +0600

    feat : Fetched data from db for dashboard

--- app/Dashboard/page.tsx
+++ app/Dashboard/page.tsx
@@ -13,15 +13,15 @@ const page = () => {
 const formattedDate = new Intl.DateTimeFormat('en-US', options).format(date);
 console.log(formattedDate)
   return (
-    <div className='p-1 py-5 md:p-8 '>
+    <div className=' px-4 py-5 md:p-8 '>
       <div className='flex justify-end'>
       <h2 className='p-2 bg-black text-white font-bold rounded-lg mb-2 shadow-xl'>{formattedDate}</h2>
       </div>
-      <Tabs defaultValue="Sales Report" className='border border-black w-[200px] md:w-full shadow-2xl'>
-        <TabsList className='text-sm'>
-          <TabsTrigger value="Sales Report">Sales Report</TabsTrigger>
-          <TabsTrigger value="Profitability Report">Profitability Report</TabsTrigger>
-          <TabsTrigger value="Customer Insights">Customer Insights</TabsTrigger>
+      <Tabs defaultValue="Sales Report" className='border border-black  md:w-full shadow-2xl'>
+        <TabsList className='flex justify-center'>
+          <TabsTrigger className='text-[10px] md:text-[15px] font-bold' value="Sales Report">Sales Report</TabsTrigger>
+          <TabsTrigger className='text-[10px] md:text-[15px] font-bold' value="Profitability Report">Profitability Report</TabsTrigger>
+          <TabsTrigger className='text-[10px] md:text-[15px] font-bold' value="Customer Insights">Customer Insights</TabsTrigger>
         </TabsList>
         <TabsContent value="Sales Report"><SalesReport/></TabsContent>
         <TabsContent value="Profitability Report">Change your password here.</TabsContent>
--- components/SalesReport.tsx
+++ components/SalesReport.tsx
@@ -2,43 +2,77 @@ import Image from 'next/image'
 import React from 'react'
 import { FcSalesPerformance } from "react-icons/fc";
 import { AiOutlineStock } from "react-icons/ai";
-import { FaMapMarkerAlt } from "react-icons/fa";
+import { MdAirlines } from "react-icons/md";
+import { MdAirplaneTicket } from "react-icons/md";
+import { MdPlace } from "react-icons/md";
+import { db } from '@/Database';
+import { sql } from 'drizzle-orm';
+import { pnr } from '@/Database/schema';
 
-const SalesReport = () => {
+const SalesReport = async () => {
+  // Revenue for last 30 days
+  const revenueResult = await db.select({
+    revenue: sql<number>`SUM(${pnr.cost} + ${pnr.markup})`,
+  })
+    .from(pnr)
+    .where(sql`TO_DATE(${pnr.dob},'YYYY-MM-DD')>= NOW() - INTERVAL '30 days'`)
+  const revenue = revenueResult[0]?.revenue ?? 0
+  // Sales for last 30 days
+  const salesResult = await db.select({
+    sales: sql<number>`COUNT(*)`
+  })
+    .from(pnr)
+    .where(sql`TO_DATE(${pnr.dob},'YYYY-MM-DD') >= NOW() - INTERVAL '30 DAYS'`)
+  const sales = salesResult[0]?.sales ?? 0
+  //Most Airline
+  const airlineResult = await db.select({
+    airline: pnr.airlines,
+    airline_image: pnr.airline_image,
+    usageCount: sql<number>`COUNT(${pnr.airlines})`,
+  })
+    .from(pnr)
+    .groupBy(pnr.airlines, pnr.airline_image)
+    .orderBy(sql`COUNT(${pnr.airlines}) DESC`)
+    .limit(1)
+  const airline = airlineResult[0]
   return (
     <div>
-      <div className='flex p-4 gap-3'>
-        <div className='bg-slate-200 shadow-xl rounded-xl p-4 w-[300px] hover:shadow-2xl'>
+      <div className='px-5'>
+          <h2 className='font-bold'>Summary Metrics</h2>
+        </div>
+      <div className='flex flex-col md:flex-row p-4 gap-3'>
+        <div className='bg-slate-200 shadow-xl rounded-xl p-4 md:w-[300px] hover:shadow-2xl border border-gray-500'>
           <div className=' gap-2 items-center'>
             <Image src={'/revenue.png'} alt='revenue' height={30} width={40} />
             <h2 className='font-bold text-gray-500'>Revenue Earned</h2>
           </div>
           <div className=' flex justify-between items-center'>
-            <h1 className='text-2xl font-bold'>BDT 25,000</h1>
+            <h1 className='text-2xl font-bold'>BDT {revenue}</h1>
             <FcSalesPerformance className='text-4xl' />
-
           </div>
+          <p className='font-bold text-sm text-gray-600'>For past 30 days</p>
         </div>
-        <div className='bg-slate-200 shadow-xl rounded-xl p-4 w-[300px] hover:shadow-2xl'>
+        <div className='bg-slate-200 shadow-xl rounded-xl p-4 md:w-[300px] hover:shadow-2xl border border-gray-500'>
           <div className=' gap-2 items-center'>
-            <Image src={'/ticket.png'} alt='revenue' height={30} width={65} />
+            <MdAirplaneTicket className='text-blue-500 text-4xl' />
             <h2 className='font-bold text-gray-500'>Tickets Sold</h2>
           </div>
           <div className=' flex justify-between items-center'>
-            <h1 className='text-2xl font-bold'>1,000</h1>
+            <h1 className='text-2xl font-bold'>{sales}</h1>
             <AiOutlineStock className='text-4xl text-green-500' />
-
           </div>
+          <p className='font-bold text-sm text-gray-600'>For past 30 days</p>
         </div>
-        <div className='bg-slate-200 shadow-xl rounded-xl p-4 w-[300px] hover:shadow-2xl'>
+        <div className='bg-slate-200 shadow-xl rounded-xl p-4 md:w-[300px] hover:shadow-2xl border border-gray-500'>
           <div className=' gap-2 items-center'>
-            <Image src={'/destination.png'} alt='revenue' height={30} width={65} />
-            <h2 className='font-bold text-gray-500'>Most Visits</h2>
+            <MdAirlines className='text-red-600 text-4xl' />
+            <h2 className='font-bold text-gray-500'>Most Used Airline</h2>
           </div>
           <div className=' flex justify-between items-center'>
-            <h1 className='text-2xl font-bold'>Bangalore</h1>
-            <FaMapMarkerAlt className='text-4xl text-red-600' />
+            <h1 className='text-2xl font-bold'>{airline.airline}</h1>
+            <Image src={airline.airline_image || '/irfexlogo.png'} alt='airline logo' height={50} width={40} />
           </div>
+          <p className='font-bold text-sm text-gray-600'>Uage Count {airline.usageCount}</p>
         </div>
       </div>
     </div>
