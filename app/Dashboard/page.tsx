77ec1562e818293922382025e0e970cb6b962e5c
devinsomniac <inzamamchowdhury20@gmail.com>
2025-01-26 22:06:53 +0600

    feat : Profit tab

--- app/Dashboard/page.tsx
+++ app/Dashboard/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import SalesReport from '@/components/SalesReport';
+import ProfitabilityReport from '@/components/ProfitabilityReport';
 
 const page = () => {
   return (
@@ -12,7 +13,7 @@ const page = () => {
           <TabsTrigger className='text-[10px] md:text-[15px] font-bold' value="Heatmap">Heat Map</TabsTrigger>
         </TabsList>
         <TabsContent value="Sales Report"><SalesReport/></TabsContent>
-        <TabsContent value="Profitability Report">Change your password here.</TabsContent>
+        <TabsContent value="Profitability Report"><ProfitabilityReport/></TabsContent>
         <TabsContent value="Heatmap">Change your password here.</TabsContent>
       </Tabs>
 
---
+++ components/ProfitabilityReport.tsx
@@ -0,0 +1,90 @@
+import React from 'react'
+import { FaFileInvoiceDollar } from "react-icons/fa";
+import { FaMoneyBillTrendUp } from "react-icons/fa6";
+import { FiTrendingUp } from "react-icons/fi";
+import { CiPercent } from "react-icons/ci";
+import { FaRoute } from "react-icons/fa6";
+import { TbRouteAltLeft } from "react-icons/tb";
+import { ProfitChart } from './ProfitChart';
+import { ProfitByAirlines } from './ProfitByAirlines';
+import { db } from '@/Database';
+import { desc, sql } from 'drizzle-orm';
+import { pnr } from '@/Database/schema';
+
+const ProfitabilityReport = async() => {
+    // Net Profit
+    const ProfitResult = await db
+    .select({
+        profit : sql<number>`SUM (${pnr.markup})`
+    })
+    .from(pnr)
+    .where(sql`TO_DATE(${pnr.dob},'YYYY-MM-DD') >= NOW() - INTERVAL '30 days'`)
+    console.log("Result is ",ProfitResult[0])
+
+    //Profit margin based on revenue
+    const ProfitMargin = await db
+    .select({
+        TotalMargin : sql<number>`(SUM(${pnr.markup}) * 100.0) / SUM(${pnr.cost} + ${pnr.markup})`
+    })
+    .from(pnr)
+    .where(sql`TO_DATE(${pnr.dob},'YYYY-MM-DD') >= NOW() - INTERVAL '30 days'`)
+    const marginValue = ProfitMargin[0].TotalMargin;
+    const formatMrgin = marginValue !== null && marginValue !== undefined 
+    ? Number(marginValue).toFixed(2) : "0.00";
+    console.log("Margin is ",formatMrgin)
+
+    //Most Visited City Till now
+    const PopularCity = await db
+    .select({
+        place : sql<string>`${pnr.arrival_address}`,
+        count : sql<number>`COUNT(${pnr.arrival_address})` 
+    })
+    .from(pnr)
+    .groupBy(sql`${pnr.arrival_address}`)
+    .orderBy(sql`COUNT(${pnr.arrival_address}) DESC`)
+    .limit(1)
+    console.log("Place is :",PopularCity[0].count)
+    return (
+        <div className='p-4'>
+            <h2 className='font-bold p-2'>Profit Summary</h2>
+            {/* KPI Card for profit */}
+            <div className='flex flex-col md:flex-row justify-evenly'>
+                <div className='shadow-xl rounded-xl p-4 md:w-[300px] hover:shadow-slate-600 border border-gray-500'>
+                    <FaFileInvoiceDollar className='text-green-600 text-3xl' />
+                    <p className='font-bold text-gray-500'>Total Profit</p>
+                    <div className='flex justify-between'>
+                        <p className='font-semibold text-2xl'>BDT {ProfitResult[0].profit}</p>
+                        <FaMoneyBillTrendUp className='text-3xl text-red-600' />
+                    </div>
+                    <p className='font-bold text-gray-500'>For last 30 days</p>
+                </div>
+                <div className='shadow-xl rounded-xl p-4 md:w-[300px] hover:shadow-slate-600 border border-gray-500'>
+                <FiTrendingUp  className='text-green-600 text-3xl' />
+                    <p className='font-bold text-gray-500'>Profit margin</p>
+                    <div className='flex justify-between'>
+                        <p className='font-semibold text-2xl'>{formatMrgin} %</p>
+                        <CiPercent  className='text-3xl text-red-600' />
+                    </div>
+                    <p className='font-bold text-gray-500'>For last 30 days</p>
+                </div>
+                <div className='shadow-xl rounded-xl p-4 md:w-[300px] hover:shadow-slate-600 border border-gray-500'>
+                <TbRouteAltLeft  className='text-green-600 text-3xl' />
+                    <p className='font-bold text-gray-500'>Popuar Destination</p>
+                    <div className='flex justify-between'>
+                        <p className='font-semibold text-xl'>{PopularCity[0].place}</p>
+                        <FaRoute  className='text-3xl text-red-600' />
+                    </div>
+                    <p className='font-bold text-gray-500 text-sm'>Total {PopularCity[0].count} PNR are issued for this place</p>
+                </div>
+            </div>
+            <h2 className='font-bold mt-4 ml-3'>Profit Trends</h2>
+            <div className='px-6 grid grid-cols-1 md:grid-cols-2 gap-2'>
+                <ProfitChart/>
+                <ProfitByAirlines/>
+            </div>
+            
+        </div>
+    )
+}
+
+export default ProfitabilityReport
